476b2793f3037e88f00ace07239c6bd6251976c9
julietacolque <yuliicolque@gmail.com>
2023-06-11 21:40:02 -0300

    Punto2

--- Program.cs
+++ Program.cs
@@ -4,25 +4,70 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
         var ListaTareas = new List<Tarea>();
-        Tarea t1 = new Tarea();
-        t1.Descripcion = "lalala";
-        t1.TareaID = 0;
-        t1.Duracion = 10;
-        ListaTareas.Add(t1);
-        foreach (var tarea in ListaTareas)
-        {
-            MostrarTarea(tarea);
-        }
+        var ListaRealizadas = new List<Tarea>();
+
+        System.Console.WriteLine("----------LISTA TAREAS----------");
+        CargarNTareas(ListaTareas, 10);
+        ListarTareas(ListaTareas);
+        ControlDeTareas(ListaTareas, ListaRealizadas);
+        System.Console.WriteLine("-----------TAREAS PENDIENTES----------");
+        ListarTareas(ListaTareas);
+        System.Console.WriteLine("----------TAREAS REALIZADAS----------");
+         ListarTareas(ListaRealizadas);
+
+
     }
     public static void MostrarTarea(Tarea tarea)
     {
+        System.Console.WriteLine("ID:" + tarea.Id);
         System.Console.WriteLine("Descripcion: " + tarea.Descripcion);
         System.Console.WriteLine("Duracion: " + tarea.Duracion);
-        System.Console.WriteLine("ID:" + tarea.TareaID);
+        System.Console.WriteLine("Estado: " + tarea.Estado);
+
     }
-//     public static Tarea CargarTarea(){
-//   return 
-//     }
+    public static Tarea CargarTarea(int indice)
+    {
+        Tarea tarea = new Tarea();
+        Random rnd = new Random();
+        Array valores = Enum.GetValues(typeof(Estado));
+        Random rnd2 = new Random();
+        tarea.Id = indice;
+        tarea.Duracion = rnd.Next(1, 100);
+        tarea.Descripcion = "Tarea" + tarea.Id;
+        tarea.Estado = (Estado)valores.GetValue(rnd2.Next(valores.Length));
+        return tarea;
+
+
+    }
+    public static void ListarTareas(List<Tarea> ListaTareas)
+    {
+        foreach (var tarea in ListaTareas)
+        {
+            System.Console.WriteLine("*****************");
+            MostrarTarea(tarea);
+        }
+    }
+
+    public static void CargarNTareas(List<Tarea> ListaTareas, int N)
+    {
+
+        for (int i = 0; i < N; i++)
+        {
+            ListaTareas.Add(CargarTarea(i));
+        }
+
+    }
+    public static void ControlDeTareas(List<Tarea> ListaTareas, List<Tarea> ListaRealizadas)
+    {
+   
+        for (int i = 0; i < ListaTareas.Count; i++)
+        {
+            if(ListaTareas[i].Estado == Estado.Realizada){
+                ListaRealizadas.Add(ListaTareas[i]);
+                ListaTareas.Remove(ListaTareas[i]);
+            }
+        }
+
+    }
 }
--- Tarea.cs
+++ Tarea.cs
@@ -2,14 +2,23 @@ namespace Tareas
 {
    public class Tarea
     {
-        private int tareaID;
-        private string? descripcion;
+        private int id;
+        private string descripcion;
         private int duracion;
-
-        public int TareaID { get => tareaID; set => tareaID = value; }
-        public string? Descripcion { get => descripcion; set => descripcion = value; }
-        public int Duracion { get => duracion; set => duracion = value; }
+        private Estado estado;
 
         
+
+       
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public int Duracion { get => duracion; set => duracion = value; }
+        public int Id { get => id; set => id = value; }
+        public Estado Estado { get => estado; set => estado = value; }
+    }
+    public enum Estado
+    {
+        Realizada,
+        EnCurso,
+        Pendiente
     }
 }
